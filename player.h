bb69a2f1c86e66a920b68146df485522ea3f1959
PeterBennyFooda <peterlin0404@gmail.com>
2013-12-24 12:40:55 +0800

    header

---
+++ player.h
@@ -0,0 +1,36 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class PLAYER
+{
+private:
+    string name;
+    int hp;
+    int mp;
+    int ap;
+    int dp;
+    int sp;
+    int exp;
+    int level;
+    string msg;
+public:
+    PLAYER(string _name="unknow", int _hp=100, int _mp=20, int _ap=34, int _dp=22)
+        :name(_name), hp(_hp), mp(_mp), ap(_ap), dp(_dp), sp(_sp), exp(_exp), level(_level) {}
+
+    void attack(PLAYER &);
+    void magic(PLAYER &);
+    void more_hp();
+    bool is_live()
+    {
+        return hp>0;
+    }
+    friend class BOX;
+    friend class GAME;
+};
+
+#endif // PLAYER_H_INCLUDED
